f9ce2c2394b079bf4e0cb0e7397ecffd7909b86f
Carlos Marco <carlosmarcovico@gmail.com>
2025-08-17 14:58:15 +0200

    Added GLSL files for default shaders, and sprite batch read it from DLL.

---
+++ RetroEngine.Graphics/Shaders/Defaults/fragment.glsl
@@ -0,0 +1,13 @@
+﻿#version 330 core
+            
+in vec2 pass_texCoord;
+in vec4 pass_color;
+            
+uniform sampler2D texture0;
+            
+out vec4 out_color;
+
+void main()
+{
+    out_color = texture(texture0, pass_texCoord) * pass_color;
+}
