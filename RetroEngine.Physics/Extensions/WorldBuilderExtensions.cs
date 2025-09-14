b7520f645eb623693682b9982accbecf4281bc53
Carlos Marco <carlosmarcovico@gmail.com>
2025-09-14 17:07:48 +0200

    fix #14: adds world builder extensions to add system bundles.

---
+++ RetroEngine.Physics/Extensions/WorldBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using OpenTK.Mathematics;
+using RetroEngine.Core;
+
+namespace RetroEngine.Physics
+{
+    /// <summary>
+    /// Defines extension method for the WorldBuilder class.
+    /// </summary>
+    public static class WorldBuilderExtensions
+    {
+        /// <summary>
+        /// Adds CollisionResolutionSystem and LinearTranslationSystem to the world builder.
+        /// </summary>
+        /// <param name="worldBuilder">World builder to add systems.</param>
+        /// <returns>Same world builder.</returns>
+        public static WorldBuilder RegisterKineticPhysicsEngine(this WorldBuilder worldBuilder)
+        {
+            return worldBuilder
+                .RegisterSystem(new CollisionResolutionSystem())
+                .RegisterSystem(new LinearTranslationSystem());
+        }
+
+        /// <summary>
+        /// Adds CollisionResolution, LinearTranslation, LinearMomentum and Gravity systems to the world builder.
+        /// </summary>
+        /// <param name="worldBuilder">World builder to add systems.</param>
+        /// <param name="gravity">Gravity of the world.</param>
+        /// <returns>Same world builder.</returns>
+        public static WorldBuilder RegisterDynamicPhysicsEngine(this WorldBuilder worldBuilder, Vector2 gravity)
+        {
+            return worldBuilder
+                .RegisterSystem(new CollisionResolutionSystem())
+                .RegisterSystem(new LinearTranslationSystem())
+                .RegisterSystem(new LinearMomentumSystem())
+                .RegisterSystem(new GravitySystem());
+        }
+    }
+}
---
+++ RetroEngine.UnitTest/Physics/Extensions/WorldBuilderExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using OpenTK.Mathematics;
+using RetroEngine.Core;
+using RetroEngine.Physics;
+
+namespace RetroEngine.UnitTest.Physics.Extensions
+{
+    /// <summary>
+    /// Defines unit test cases for WorldBuilderExtensions class.
+    /// </summary>
+    public class WorldBuilderExtensionsTests
+    {
+        [Fact]
+        public void WorldBuilder_RegisterKineticPhysics_AddsLinearTranslationAndCollisionResolutionSystems()
+        {
+            // Arrange
+            var worldBuilder = new WorldBuilder();
+
+            // Act
+            var world = worldBuilder
+                .RegisterKineticPhysicsEngine()
+                .Build();
+
+            // Act
+            var entity = world.CreateEntity();
+
+            Assert.Null(Record.Exception(() => entity.Attach(new Transform())));
+            Assert.Null(Record.Exception(() => entity.Attach(new Collider2D())));
+            Assert.Null(Record.Exception(() => entity.Attach(new LinearPhysics2D())));
+        }
+
+        [Fact]
+        public void WorldBuilder_RegisterDynamicPhysics_AddsLinearTranslationAndCollisionResolutionSystems()
+        {
+            // Arrange
+            var worldBuilder = new WorldBuilder();
+
+            // Act
+            var world = worldBuilder
+                .RegisterDynamicPhysicsEngine(Vector2.UnitY * -9.8f)
+                .Build();
+
+            // Act
+            var entity = world.CreateEntity();
+
+            Assert.Null(Record.Exception(() => entity.Attach(new Transform())));
+            Assert.Null(Record.Exception(() => entity.Attach(new Collider2D())));
+            Assert.Null(Record.Exception(() => entity.Attach(new LinearPhysics2D())));
+            Assert.Null(Record.Exception(() => entity.Attach(new RigidBody())));
+        }
+    }
+}
